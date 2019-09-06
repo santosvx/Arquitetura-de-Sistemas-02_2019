155d6e69388d044c1578d8952bf105f593517dc1
Vinicius George dos Santos <32928734+santosvx@users.noreply.github.com>
2019-09-05 22:26:09 -0300

    Add files via upload
    Aplicação
    
    1.       Crie uma classe para representar uma pessoa, com os atributos privados de nome, idade e altura. Crie os métodos públicos necessários para sets e gets e também um método para imprimir os dados de uma pessoa.
    2.       Crie uma classe denominada Elevador para armazenar as informações de um elevador dentro de um prédio. A classe deve armazenar o andar atual (térreo = 0), total de andares no prédio, excluindo o térreo, capacidade do elevador, e quantas pessoas estão presentes nele.

---
+++ Exemplo005.java
@@ -0,0 +1,32 @@
+package br.com.una.santos.george.vinicius.aula0509;
+
+public class Exemplo005 {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+}
+}
+
+/*
+Exercício de Fixação
+
+1.       Crie uma classe para representar uma pessoa, com os atributos privados de nome, idade e altura. Crie os métodos públicos necessários para sets e gets e também um método para imprimir os dados de uma pessoa.
+2.       Crie uma classe denominada Elevador para armazenar as informações de um elevador dentro de um prédio. A classe deve armazenar o andar atual (térreo = 0), total de andares no prédio, excluindo o térreo, capacidade do elevador, e quantas pessoas estão presentes nele.
+ 
+A classe deve também disponibilizar os seguintes métodos:
+
+·         Inicializa: que deve receber como parâmetros a capacidade do elevador e o total de andares no prédio (os elevadores sempre começam no térreo e vazio);
+·         Entra: para acrescentar uma pessoa no elevador (só deve acrescentar se ainda houver espaço);
+·         Sai: para remover uma pessoa do elevador (só deve remover se houver alguém dentro dele);
+·         Sobe: para subir um andar (não deve subir se já estiver no último andar);
+·         Desce: para descer um andar (não deve descer se já estiver no térreo);
+ 
+ 
+Atenção: Deve-se encapsular todos os atributos da classe (criar os métodos set e get).
+
+*/
+	
+/*
+Metodologia Agil: Fazer o minimo, que entrega valor, testar, depois refatorar.
+*/
