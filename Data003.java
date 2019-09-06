192564c3cc452cbe502471d0ec6e56c72bed65cc
Vinicius George dos Santos <32928734+santosvx@users.noreply.github.com>
2019-09-05 21:56:05 -0300

    Add files via upload
    change attribute to private

--- Data003.java
+++ Data003.java
@@ -2,7 +2,7 @@ package br.com.una.santos.george.vinicius.aula0509;
 
 public class Data003 {
 
-	public int dia, mes, ano;
+	private int dia, mes, ano;
 	
 	public void iniciaData (int d, int m, int a) {
 		dia = (d > 0) && (d <= 31)? d : 1;
