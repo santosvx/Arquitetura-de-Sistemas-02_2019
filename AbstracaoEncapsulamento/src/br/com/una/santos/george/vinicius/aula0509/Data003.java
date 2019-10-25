2d7fb0a1ab85c9bcd67327e007678f3a4f5ee913
Vinicius George dos Santos <32928734+santosvx@users.noreply.github.com>
2019-10-24 21:38:48 -0300

    Abstração e Encapsulamento

---
+++ AbstracaoEncapsulamento/src/br/com/una/santos/george/vinicius/aula0509/Data003.java
@@ -0,0 +1,21 @@
+package br.com.una.santos.george.vinicius.aula0509;
+
+public class Data003 {
+
+	private int dia, mes, ano;
+	
+	public void iniciaData (int d, int m, int a) {
+		dia = (d > 0) && (d <= 31)? d : 1;
+		mes = (m > 0) && (m <= 12)? m : 1;
+		ano = a > 0 ? a : 2000;
+		//ano = (a > 0)? a : 2000;
+	}
+	public void exibe ( ) { System.out.println(dia + " / " + mes + " / " + ano);
+}
+	public void setDia (int d) { dia = d; }
+	public void setMes (int m) { dia = m; }
+	public void setAno (int a) { dia = a; }
+	public int getDia ( ) { return dia; }
+	public int getMes ( ) { return mes; }
+	public int getAno ( ) { return ano; }
+}
